325041d660eba944aaf0fccba4711dd1809490e6
Ayato <ay4toh5i@gmail.com>
2025-10-26 23:25:04 +0900

    Add deps.ts to cache dependencies

---
+++ deps.ts
@@ -0,0 +1,5 @@
+import "textlint";
+import "textlint-filter-rule-comments";
+import "textlint-rule-preset-ja-technical-writing";
+import "@textlint-ja/textlint-rule-preset-ai-writing";
+import "@proofdict/textlint-rule-proofdict";
